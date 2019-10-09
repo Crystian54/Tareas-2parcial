ba9de7e268fe0b1b8bccb5535f7b0d3ee5c3b4fe
Crystian Chavez <55474854+Crystian54@users.noreply.github.com>
2019-10-08 23:30:57 -0500

    Add files via upload

---
+++ 1.1.9.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+ int x,z;
+ x=2;
+ if(3!=x){
+ 	if(x==1){
+ 		z=1;
+ 	}
+ 	else{
+ 		z=x;
+ 	}
+ }
+ else{
+ 	z=0;
+ }
+ printf("el resultado de z es %d",x);
+}
