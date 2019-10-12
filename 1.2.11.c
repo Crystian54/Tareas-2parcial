68557bc736efd6920ab2717d0ba6ddf22b988201
Crystian Chavez <55474854+Crystian54@users.noreply.github.com>
2019-10-11 23:32:44 -0500

    Add files via upload

---
+++ 1.2.11.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void){
+float n,max=0,min=(n*n),i,c=0,m;
+printf("Maximo,minimo y media de una lista de 10 numeros.\n");
+for(i=1;i<=10;i++){
+  printf("digita el valor\t");
+  scanf("%f",&n);
+  if(n>max){
+  max=n;
+  }
+  if(n<min){
+  min=n;
+  }
+  c=c+n;
+ }
+m=c/10;
+printf("El numero mayor es %f y el menor es %f.\nLa media es: %f.",max,min,m);
+}
