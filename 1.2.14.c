dcc43f4174b679ae8c9e8f6ceae2ba51445111ba
Crystian Chavez <55474854+Crystian54@users.noreply.github.com>
2019-10-12 00:30:07 -0500

    Add files via upload

---
+++ 1.2.14.c
@@ -0,0 +1,26 @@
+/*Crystian Alonso Chavez Jimenez,1AV6 ejercicio 14*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+int main (void){
+    int v;
+    char n,o;
+    do{
+    printf("\nEscribe una letra, cual sea\t"); scanf("%c",&n);
+    if('a'==n || 'A'==n || 'e'==n || 'E'==n || 'i'==n || 'I'==n || 'o'==n || 'O'==n || 'u'==n || 'O'==n){
+        v=1;
+    }
+    else{
+        v=2;
+    }
+    if(v==1){
+        printf("\nLa letra es vocal");
+    }
+    else{
+        printf("\nLa letra es consonante");
+    }
+    printf("\nDeseas llevar a acabo otra comparacion\t");
+    printf("\n S-SI\n N-NO\n"); scanf("%c",&o);
+    o=getch();
+    }while('s'==o||'S'==o);
+}
